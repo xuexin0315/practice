57850a3d8da90389fe9a2856e2eb8746620d753d
xuexin0315 <wxx159646>
2020-06-04 19:52:57 +0800

    测试

--- src/main/java/practice/Topic1.java
+++ src/main/java/practice/Topic1.java
@@ -12,20 +12,24 @@ public class Topic1
 {
     public static void main(String[] args) {
 
+
         ArrayList<Integer> list = new ArrayList<>();
+
         int[] arr = new int[10];
-        Random ra = new Random();
-        for (int i =0;i < arr.length;i++)
-        {
-            arr[i] = ra.nextInt(100)+1;//为什么加一
-            if(arr[i]>=10)
-            {
+        Random ra =new Random();
+        for (int i = 0; i <arr.length ; i++) {
+
+            arr[i]=ra.nextInt(100)+1;
+            if(arr[i]>=10){
                 list.add(arr[i]);
+
             }
 
         }
-        System.out.println(list);
 
+        System.out.println(list);
     }
+
+
 
 }
